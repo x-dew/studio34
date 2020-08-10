a3bccb299c722d9037fb6d860d97930693767aec
itop55 <itop55@gmail.com>
2020-08-10 11:24:53 +0300

    main menu finish

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,10 @@
+// Импортируем jQuery
+import "jquery";
+// Импортируем необходимые js-файлы Bootstrap 4
+
+import "bootstrap/js/dist/util";
+import "bootstrap/js/dist/collapse";
+import "bootstrap/js/dist/dropdown";
+
+// Импортируем другие js-файлы
 import "./import/modules";
