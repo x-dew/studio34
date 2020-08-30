c1b35916f95e93e518066fe85af9248eca55902a
itop55 <bachida.imihi@gmail.com>
2020-08-30 06:23:46 +0300

    delete contact block script

--- src/blocks/modules/contact-form/contact-form.js
+++ src/blocks/modules/contact-form/contact-form.js
@@ -10,32 +10,3 @@ $(document).ready(function () {
     });
 });
 /*Contact form script*/
-
-/* Contact form image animation script*/
-var block_show = false;
-
-function scrollTracking(){
-    if (block_show) {
-        return false;
-    }
-
-    var wt = $(window).scrollTop();
-    var wh = $(window).height();
-    var et = $('.contact-form__wrapper').offset().top;
-    var eh = $('.contact-form__wrapper').outerHeight();
-    var dh = $(document).height();
-
-    if (wt + wh >= et || wh + wt == dh || eh + et < wh){
-        block_show = true;
-        $('.contact-form__animation').addClass('animation-start');
-    }
-}
-
-$(document).scroll(function(){
-    scrollTracking();
-});
-
-$(document).ready(function(){
-    scrollTracking();
-});
-/* Contact form image animation script*/
